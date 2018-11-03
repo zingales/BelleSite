603d96767d2150faf09ffc92876e4c271b5570d2
belle <belle@noemail.com>
2018-11-02 22:30:20 -0700

    added painting page

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/painting', (req, res) => {
+  res.render('painting');
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// "/inquiries"
+// "/social"
+// "/about"
+// "/photography"
+//
+// "/sculpture"
+// "/mixedmedia"
+// "/shop"
+// "/blog"
